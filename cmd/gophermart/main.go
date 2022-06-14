43c7603c6529bf5f14f0018aee003f1c94c71ff7
Kello <kello-easy@mail.ru>
2022-06-14 03:00:24 +0000

    yolo

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func ping(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("pong")
+}
 
 func main() {
 	fmt.Println("IM HERE!")
+	time.Sleep(time.Second*5)
+	http.HandleFunc("/api/user/register", ping)
+	http.ListenAndServe(":8080", nil)
 }
